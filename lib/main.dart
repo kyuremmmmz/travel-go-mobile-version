af4c65e15683e90a85100038e4179a0d760da307
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-08-08 21:09:00 +0800

    to master

--- lib/main.dart
+++ lib/main.dart
@@ -9,36 +9,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    return  MaterialApp(
+      title: 'i transit',
+      home: Scaffold( 
         appBar: AppBar(
-          title: const Text('I transition'),
-          shadowColor: Colors.black,
-          backgroundColor: Colors.white,
-        ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: Colors.black54,
-            border:
-            Border.all(
-              color: Colors.black45,
-              width: 500,
-              ),
-              borderRadius: BorderRadius.circular(100),
-            ),
-            child: const Text(
-              'Hello world',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ),
+          title: const Text('i transit'),
         ),
       ),
     );
