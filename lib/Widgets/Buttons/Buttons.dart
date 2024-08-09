e05c273f9fb32b6ed2b654cc4fd512c474d0f55b
Christian David Jasmin <kurosawataki84@gmail.com>
2024-08-09 20:44:16 +0800

    added some button color

--- lib/Widgets/Buttons/Buttons.dart
+++ lib/Widgets/Buttons/Buttons.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 
 class Buttonns extends StatefulWidget {
-  const Buttonns({super.key});
+  late final String text;
+  late final Color initialColor;
 
+  Buttonns({required this.text, this.initialColor = Colors.green});
   @override
   State<Buttonns> createState() => _ButtonnsState();
+
+
 }
 
 class _ButtonnsState extends State<Buttonns> {
+  late Color haha;
+  @override
+  void initState(){
+    super.initState();
+    haha = widget.initialColor;
+  }
+
+  void setter() {
+    setState(() {
+      haha = haha ==  Colors.green ? Colors.red : Colors.green;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(
+      onPressed: , 
+      child: Text(widget.text));
   }
 }
