30b6d02a696de79cd8fbc056865211e444808eef
Christian David Jasmin <kurosawataki84@gmail.com>
2024-08-09 19:19:12 +0800

    added some buttons widget

---
+++ lib/Widgets/Buttons/Buttons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Buttonns extends StatefulWidget {
+  const Buttonns({super.key});
+
+  @override
+  State<Buttonns> createState() => _ButtonnsState();
+}
+
+class _ButtonnsState extends State<Buttonns> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
