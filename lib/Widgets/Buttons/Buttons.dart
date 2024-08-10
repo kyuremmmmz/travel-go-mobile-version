1bab79c7ff0cbea88f230820678f4d4ece43e994
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-08-10 21:48:51 +0800

    just added some shits

--- lib/Widgets/Buttons/Buttons.dart
+++ lib/Widgets/Buttons/Buttons.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class Buttonns extends StatefulWidget {
   late final String text;
   late final Color initialColor;
+  late final String textDisplay;
 
-  Buttonns({required this.text, this.initialColor = Colors.green});
+  Buttonns({required this.text, this.initialColor = Colors.green, required this.textDisplay});
   @override
   State<Buttonns> createState() => _ButtonnsState();
 
@@ -13,21 +14,28 @@ class Buttonns extends StatefulWidget {
 
 class _ButtonnsState extends State<Buttonns> {
   late Color haha;
+  late String buttontext;
   @override
   void initState(){
     super.initState();
     haha = widget.initialColor;
+    buttontext = widget.textDisplay;
   }
 
   void setter() {
     setState(() {
       haha = haha ==  Colors.green ? Colors.red : Colors.green;
+      buttontext = haha == Colors.green ? "buttonpressed" : "button";
       print('button pressed');
+      
     });
   }
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.yellowAccent,
+      ),
       onPressed: setter, 
       child: Text(widget.text)
       );
