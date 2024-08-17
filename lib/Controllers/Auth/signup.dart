fe1abd43593ce8e245dac9fcc5e4690baed8403a
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-08-17 18:56:11 +0800

    to master

--- lib/Controllers/Auth/signup.dart
+++ lib/Controllers/Auth/signup.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class Signup {
+  late final String? lastName;
+  late final String? firstName;
+  late final int? phoneNumber;
+  late final int? age;
+  late final String? address;
+  late final String? email;
+  late final String? password;
 
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
+  Signup(
+    {
+      required this.email,
+      required this.password,
+    }
+  );
 
-class _MyWidgetState extends State<MyWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Future<Map<String, dynamic>> signUp() async {
+    final supabase = Supabase.instance.client;
+    try {
+        final AuthResponse response = await supabase.auth.signUp(
+          email: email!,
+          password: password!,
+        );
+          final Session? session = response.session;
+          final User? user = response.user;
+        if (email != null && user != null) {
+          return {
+            'status': 200,
+            'message': 'success',
+            'data': {
+              'user': user.email,
+              'id': user.id,
+              'adminOrNot': user.role == 'admin'? "you're in admin mode" : "you're in user mode"
+            }
+          };
+        }
+    // ignore: empty_catches
+    } catch (error) {
+      
+    }
   }
 }
+
