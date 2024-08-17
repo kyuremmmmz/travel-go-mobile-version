948d1f67eca051e69ce49f159efc8dfe0269f736
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-08-17 19:34:53 +0800

    home screen

---
+++ lib/Widgets/Screens/App/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  void main(){
+    runApp(const Home());
+  }
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        
+    );
+  }
+}
