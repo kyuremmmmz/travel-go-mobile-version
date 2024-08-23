b5dd231a529a76f025fcd6cc11ffce5f5c1cc30c
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-08-23 21:05:45 +0800

    added forgot password screens

---
+++ lib/Widgets/Screens/Stateless/forgot.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:itransit/Widgets/Screens/Auth/Forgot.dart';
+class Forgotpassword extends StatelessWidget {
+  const Forgotpassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'forgot password page',
+      home: Forgotpassword(),
+    );
+  }
+}
