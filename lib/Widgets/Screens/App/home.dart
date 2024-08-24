0a626b20bdcf37ce3178fa6a463c7cd9427aab52
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-08-24 22:07:01 +0800

    database for booking

--- lib/Widgets/Screens/App/home.dart
+++ lib/Widgets/Screens/App/home.dart
@@ -65,7 +65,7 @@ class _HomeState extends State<Home> {
                           Usersss().signout();
                       },
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red
+                      backgroundColor: Colors.white
                     ),
                 text: const Text
                 (
