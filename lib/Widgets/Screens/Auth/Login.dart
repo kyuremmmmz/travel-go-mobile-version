6494990b971e8ef5160f7c6f623ef989e7a24fa5
Christian David Jasmin <kurosawataki84@gmail.com>
2024-08-29 21:11:10 +0800

    fixed the bug of sign out

--- lib/Widgets/Screens/Auth/Login.dart
+++ lib/Widgets/Screens/Auth/Login.dart
@@ -175,7 +175,7 @@ class _LoginScreenState extends State<LoginScreen> {
                             ),
                           ),
                           oppressed: () async {
-                            Login(
+                            await Login(
                                     email: _emailController.text.trim(),
                                     password: _passwordController.text.trim())
                                 .loginUser(context);
