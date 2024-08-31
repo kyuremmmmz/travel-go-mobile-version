7c808ea8957c7a7703f9731f8ba7129b5fa3b39d
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-08-31 20:28:43 +0800

    huwag muna may redline pa

--- lib/Widgets/Screens/Auth/Forgot.dart
+++ lib/Widgets/Screens/Auth/Forgot.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:itransit/Widgets/Buttons/DefaultButtons/BlueButton.dart';
 import 'package:itransit/Widgets/Textfield/searchField.dart';
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
@@ -40,6 +41,7 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
                 const SizedBox(
                   height: 30,
                 ),
+                // ignore: avoid_unnecessary_containers
                 Container(
                   child: const Text(
                     textAlign: TextAlign.center,
@@ -70,8 +72,22 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
                   const SizedBox(
                     height: 60,
                   ),
+                  // ignore: sized_box_for_whitespace
                   Container(
-                    width: 400,
+                    width: 350,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50)
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          spreadRadius: 0.3,
+                          blurRadius: 12.0,
+                          offset: Offset(0, 6.0)
+                        )
+                      ]
+                    ),
                     child: Search(
                           controller: _emailController,
                           style: const InputDecoration(
@@ -79,16 +95,29 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
                               borderRadius: BorderRadius.all(
                                 Radius.circular(50)
                               ),
-                              borderSide: BorderSide(color: Colors.black),
+                              borderSide: BorderSide(color: Colors.white),
                             ),
                             focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.black)),
-                            hintText: "Enter your email",
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(50)
+                                )
+                              ),
+                            hintText: "Email Address",
                             filled: true,
                             fillColor: Colors.white,
                           )
                         ),
-                      )
+                      ),
+                      const SizedBox(
+                        height: 150,
+                      ),
+                      BlueButtonWithoutFunction(
+                        text: const Text('Send Instructions'), 
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white
+                        ),
+                        oppressed: oppressed)
                     ],
                   ),
                 )
