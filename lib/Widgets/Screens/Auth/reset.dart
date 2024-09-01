73914780b0306c4c833ed70ad0fb7492da78bc53
Christian David Jasmin <kurosawataki84@gmail.com>
2024-09-01 18:28:53 +0800

    adding backend and the widgets

--- lib/Widgets/Screens/Auth/reset.dart
+++ lib/Widgets/Screens/Auth/reset.dart
@@ -10,6 +10,28 @@ class ResetPassword extends StatefulWidget {
 class _ResetPasswordState extends State<ResetPassword> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+              right: 70,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 150,
+                  ),
+                  Container(
+                      child: RichText(
+                          text: const TextSpan(children: <TextSpan>[
+                    TextSpan(
+                        text: 'Create New Password',
+                        style: TextStyle(color: Colors.black, fontSize: 30))
+                  ]))),
+                ],
+              ))
+        ],
+      ),
+    );
   }
 }
--- lib/Widgets/Screens/Stateless/email.dart
+++ lib/Widgets/Screens/Stateless/email.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:itransit/Widgets/Screens/Auth/Email.dart';
+
 // ignore: camel_case_types
 class email extends StatelessWidget {
   const email({super.key});
@@ -7,6 +8,7 @@ class email extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: CheckEmailScreen(),
     );
   }
