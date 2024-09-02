fd3d1f65f83c718e43c72b811c3d5a3a67093df8
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-09-03 01:13:56 +0800

    home screen done

--- lib/Widgets/Screens/App/mainmenu.dart
+++ lib/Widgets/Screens/App/mainmenu.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:itransit/Widgets/Buttons/WithMethodButtons/BlueIconButton.dart';
 import 'package:itransit/Widgets/Buttons/WithMethodButtons/PlaceButtonSquare.dart';
 import 'package:itransit/Widgets/Textfield/searchField.dart';
+import 'package:itransit/Controllers/Profiles/ProfileController.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() {
   runApp(const MainMenu());
@@ -14,6 +16,7 @@ class MainMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Travel',
       home: MainMenuScreen(),
     );
   }
@@ -32,22 +35,119 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
   final String hotelIcon = "assets/images/icon/hotel.png";
   final String hundredIsland = "assets/images/places/HundredIsland.jpeg";
   final _searchController = TextEditingController();
+  String? email;
+  late Usersss users = Usersss();
+
+  Future<void> emailFetching() async {
+    try {
+      final PostgrestList useremail = await users.fetchUser();
+      if (useremail.isNotEmpty) {
+        setState(() {
+          email = useremail[0]['full_name'].toString();
+        });
+      } else {
+        setState(() {
+          email = "Anonymous User";
+        });
+      }
+    } catch (e) {
+      setState(() {
+        email = "error: $e";
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    emailFetching();
+  }
 
   @override
   void dispose() {
     _searchController.dispose();
     super.dispose();
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 40,
+        leading: Builder(
+          builder: (BuildContext context) => IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const CircleAvatar(
+                    backgroundImage: AssetImage(
+                        'assets/images/icon/beach.png'), // Replace with your own profile image
+                    radius: 40,
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    // ignore: unnecessary_null_comparison
+                    email !=null ? '$email' : 'Hacked himala e',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.search),
+              title: const Text('Search'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Logout'),
+              onTap: () {
+                Navigator.pop(context);
+                Usersss().signout(context);
+              },
+            ),
+          ],
+        ),
+      ),
       body: Stack(
         children: [
           Positioned.fill(
             child: Column(
               children: <Widget>[
-                const SizedBox(height: 50),
                 Text(
                   'TRAVEL GO',
                   style: TextStyle(
@@ -76,33 +176,16 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
                   child: Search(
                     controller: _searchController,
                     style: const InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(
-                        vertical: 0,
-                        horizontal: 10
-                        ),
-                      hintStyle: TextStyle(
-                        color: Colors.black54
-                      ),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                      hintStyle: TextStyle(color: Colors.black54),
                       hintText: 'Search Destination',
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(
-                            50
-                            )
-                          ),
-                        borderSide: BorderSide(
-                          color: Colors.black54
-                        )
-                      ),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          borderSide: BorderSide(color: Colors.black54)),
                       focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black54
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(
-                          50
-                          )
-                        )
-                      ),
+                          borderSide: BorderSide(color: Colors.black54),
+                          borderRadius: BorderRadius.all(Radius.circular(50))),
                       filled: true,
                       fillColor: Colors.white,
                     ),
