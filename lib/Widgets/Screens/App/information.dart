ea319642e7098d1412e2be213ef0274deb02fa98
unknown <peterpagasartonga@gmail.com>
2024-10-08 19:26:53 +0800

    Updated titlesearchMenu to few screens

--- lib/Widgets/Screens/App/information.dart
+++ lib/Widgets/Screens/App/information.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:TravelGo/Widgets/Screens/App/titleSearchMenu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
@@ -190,74 +191,10 @@ class _InformationScreenState extends State<InformationScreen> {
               } else {
                 return Stack(
                   children: [
-                    Positioned(
+                    const Positioned(
                       child: Column(
                         children: <Widget>[
-                          Text(
-                            'TRAVEL GO',
-                            style: TextStyle(
-                              fontSize: 30.sp, // reponsive text
-                              color: Colors.blue,
-                              fontWeight: FontWeight.bold,
-                              shadows: [
-                                Shadow(
-                                  offset: const Offset(3.0, 3.0),
-                                  blurRadius: 4.0,
-                                  color: Colors.black.withOpacity(0.5),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const Text(
-                            "Northwestern part of Luzon Island, Philippines",
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          const SizedBox(height: 30),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 25),
-                            child: TypeAheadField(
-                              textFieldConfiguration: TextFieldConfiguration(
-                                controller: _searchController,
-                                decoration: const InputDecoration(
-                                  contentPadding: EdgeInsets.symmetric(
-                                      vertical: 0, horizontal: 10),
-                                  hintStyle: TextStyle(color: Colors.black54),
-                                  hintText: 'Search Destination',
-                                  border: OutlineInputBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(50)),
-                                    borderSide:
-                                        BorderSide(color: Colors.black54),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.black54),
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(50)),
-                                  ),
-                                  filled: true,
-                                  fillColor: Color.fromARGB(255, 255, 255, 255),
-                                ),
-                              ),
-                              suggestionsCallback: (pattern) async {
-                                return await Searchcontroller()
-                                    .fetchSuggestions(pattern);
-                              },
-                              itemBuilder: (context, dynamic suggestion) {
-                                return ListTile(
-                                  title:
-                                      Text(suggestion['title'] ?? 'No title'),
-                                  subtitle: Text(
-                                      suggestion['address'] ?? 'No address'),
-                                );
-                              },
-                              onSuggestionSelected: (dynamic suggestion) {
-                                _searchController.text =
-                                    suggestion['title'] ?? 'No title';
-                                FocusScope.of(context).unfocus();
-                              },
-                            ),
-                          ),
+                          TitleSearchMenu(),
                         ],
                       ),
                     ),
@@ -833,9 +770,8 @@ class _InformationScreenState extends State<InformationScreen> {
                                                                         }),
                                                                         Text(
                                                                           ' $ratings OUT OF 5',
-                                                                          style: const TextStyle(
-                                                                            fontSize: 12
-                                                                          ),
+                                                                          style:
+                                                                              const TextStyle(fontSize: 12),
                                                                         ),
                                                                       ],
                                                                     ),
@@ -919,11 +855,8 @@ class _InformationScreenState extends State<InformationScreen> {
                                                       context,
                                                       MaterialPageRoute(
                                                           builder: (context) =>
-                                                              const Flight()
-                                                    )
-                                                  );
-                                                }
-                                              ),
+                                                              const Flight()));
+                                                }),
                                           )
                                         ],
                                       )
@@ -939,8 +872,6 @@ class _InformationScreenState extends State<InformationScreen> {
                   ],
                 );
               }
-            }
-          )
-        );
+            }));
   }
 }
