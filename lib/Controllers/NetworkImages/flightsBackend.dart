7a677ffd6e1d0ad1fad8cdd88ea2a9bff2d909f9
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-10-11 13:59:08 +0800

    added some backend

--- lib/Controllers/NetworkImages/flightsBackend.dart
+++ lib/Controllers/NetworkImages/flightsBackend.dart
@@ -3,8 +3,19 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class Flightsbackend {
   final supabase = Supabase.instance.client;
 
-  Future<List<Map<String, dynamic>>> flightList(int id) async {
-    final response = supabase.from('flightsList').select('*').eq('id', id);
+  Future<List<Map<String, dynamic>>> flightList() async {
+    final response = supabase.from('flightsList').select('*').eq('ticket_type', 'cheapest');
+    return [];
+  }
+
+  Future<List<Map<String, dynamic>>> flightListBest() async {
+    final response =
+        supabase.from('flightsList').select('*').eq('ticket_type', 'best');
+    return [];
+  }
+
+  Future<List<Map<String, dynamic>>> flightListFastest() async {
+    final response = supabase.from('flightsList').select('*').eq('ticket_type', 'best');
     return [];
   }
 }
