fff50a8353641ffb61291cc2285e836b5f1715c3
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-10-11 16:33:04 +0800

    added airport tangina gago puta ayaw na pilay na ako

--- lib/Controllers/NetworkImages/flightsBackend.dart
+++ lib/Controllers/NetworkImages/flightsBackend.dart
@@ -1,21 +1,53 @@
+import 'package:intl/intl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Flightsbackend {
   final supabase = Supabase.instance.client;
 
   Future<List<Map<String, dynamic>>> flightList() async {
-    final response = supabase.from('flightsList').select('*').eq('ticket_type', 'cheapest');
+    final response =
+        supabase.from('flightsList').select('*').eq('ticket_type', 'cheapest');
     return [];
   }
 
   Future<List<Map<String, dynamic>>> flightListBest() async {
+    final response = await supabase
+        .from('flightsList')
+        .select('*')
+        .eq('ticket_type', 'best');
+    if (response.isEmpty) {
+      return [];
+    } else {
+      final data = response;
+      final List<Map<String, dynamic>> results =
+          List<Map<String, dynamic>>.from(data as List);
+      for (var datas in results) {
+        var img = datas['airplane_img'];
+        var name = datas['airport'];
+        var pricePlace = datas['price'];
+        var price = NumberFormat('#,###');
+        var format = price.format(pricePlace);
+        datas['airport'] = name;
+        datas['price'] = format;
+        var imgUrl = await getter(img);
+        datas['airplane_img'] = imgUrl;
+      }
+      return data;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> flightListFastest() async {
     final response =
         supabase.from('flightsList').select('*').eq('ticket_type', 'best');
     return [];
   }
 
-  Future<List<Map<String, dynamic>>> flightListFastest() async {
-    final response = supabase.from('flightsList').select('*').eq('ticket_type', 'best');
-    return [];
+  Future<String?> getter(String name) async {
+    final response = supabase.storage.from('flights').getPublicUrl(name);
+    if (response.isEmpty) {
+      return null;
+    } else {
+      return response;
+    }
   }
 }
