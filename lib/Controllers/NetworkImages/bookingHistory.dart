3a616ca3542a79f785d671a2204de42b1a032d3f
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-10-18 18:36:53 +0800

    done na booking history

--- lib/Controllers/NetworkImages/bookingHistory.dart
+++ lib/Controllers/NetworkImages/bookingHistory.dart
@@ -39,4 +39,59 @@ class Bookinghistory {
     }
     return null;
   }
+
+  Future<PostgrestMap?> getPlace(String hotel) async {
+    try {
+      final response = await supabase
+          .from('hotels')
+          .select('hotel_located')
+          .eq('hotel_name', hotel)
+          .single();
+      if (response.isNotEmpty) {
+        final location = response;
+        final located = location['hotel_located'];
+        location['hotel_located'] = located;
+        return location;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      SnackBar(content: Text('error: $e'));
+      return null;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getBookingHistory() async {
+    try {
+      final user = supabase.auth.currentUser;
+      final response =
+          supabase.from('booking_history').select('*').eq('id', user!.id);
+      if (response.toString().isNotEmpty) {
+        final data = response;
+        final List<Map<String, dynamic>> dataList =
+            List<Map<String, dynamic>>.from(data as List);
+        for (var datas in dataList) {
+          final price = datas['price'];
+          final hotel = datas['hotel'];
+          final checkIn = datas['checkin'];
+          final checkOut = datas['checkout'];
+          final room = datas['room_type'];
+          final bookingId = datas['booking_id'];
+          final paymentStatus = datas['paymet_status'];
+          datas['price'] = price;
+          datas['hotel'] = hotel;
+          datas['checkin'] = checkIn;
+          datas['checkout'] = checkOut;
+          datas['room_type'] = room;
+          datas['booking_id'] = bookingId;
+          datas['payment_status'] = paymentStatus;
+        }
+        return dataList;
+      } else {
+        return [];
+      }
+    } catch (e) {
+      return [];
+    }
+  }
 }
