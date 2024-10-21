5371379e76435f7b16c7461cccf41aedad7fd430
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-10-21 20:06:03 +0800

    to main

--- lib/Controllers/SearchController/searchController.dart
+++ lib/Controllers/SearchController/searchController.dart
@@ -1,24 +1,21 @@
 import 'dart:convert';
 
+import 'package:TravelGo/main.dart';
 import 'package:http/http.dart' as http;
+
 class Searchcontroller {
   Future<List<dynamic>> fetchSuggestions(String query) async {
-    final headers = {
-      'X-API-KEY': '2ed3f8f207ac5be7669b246d2924381911403f34',
-      'Content-Type': 'application/json',
-    };
-
     try {
-      final response = await http.post(
-        Uri.parse('https://google.serper.dev/places'),
-        headers: headers,
-        body: json.encode({"q": query, "location": "Philippines", "gl": "ph"}),
-      );
+      final response = await supabase.from('places').select('*');
 
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body) as Map<String, dynamic>;
-        final places = data['places'] as List<dynamic>;
-        return places;
+      if (response.isNotEmpty) {
+        final data = response;
+        List datas = List.from(data);
+        for (var quesries in datas) {
+          final place = quesries['place_name'];
+          quesries['place_name'] = place;
+        }
+        return datas;
       } else {
         throw Exception('Failed to load suggestions');
       }
