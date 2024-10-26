4dbcd3bc11a799b1333b9eb4043b15e6721c08a9
Pitz1600 <peterpagasartonga@gmail.com>
2024-10-26 18:47:58 +0800

    Responsive for orderReceipt

--- lib/Widgets/TextWidgets/rowDetails.dart
+++ lib/Widgets/TextWidgets/rowDetails.dart
@@ -1,30 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 // ignore: must_be_immutable
-class RowDetails extends StatelessWidget{
+class RowDetails extends StatelessWidget {
   String row1, row2;
-  RowDetails({super.key, 
-    required this.row1, 
+  RowDetails({
+    super.key,
+    required this.row1,
     required this.row2,
   });
 
   @override
   Widget build(BuildContext context) {
-  return Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
           row1,
-          style: const TextStyle(
+          style: TextStyle(
             fontWeight: FontWeight.bold,
-            fontSize: 10,
+            fontSize: 10.sp,
           ),
         ),
         Text(
           row2,
-          style: const TextStyle(
+          style: TextStyle(
             fontWeight: FontWeight.bold,
-            fontSize: 10,
+            fontSize: 10.sp,
             color: Color.fromRGBO(5, 103, 180, 1),
           ),
         ),
