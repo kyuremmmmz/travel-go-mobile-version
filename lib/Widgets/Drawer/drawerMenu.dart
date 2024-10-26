4b6b753d09a6e79f89aaabd4e2533b5e7c2129a8
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-10-26 16:46:02 +0800

    conflict will be resolve

--- lib/Widgets/Drawer/drawerMenu.dart
+++ lib/Widgets/Drawer/drawerMenu.dart
@@ -74,11 +74,7 @@ class _DrawerMenuWidgetState extends State<DrawerMenuWidget> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   CircleAvatar(
-<<<<<<< HEAD
                     radius: 40.sp,
-=======
-                    radius: 50,
->>>>>>> e46e9351476bb326be13f4b6e6a6b2f4067a0107
                     backgroundImage: img == null
                         ? const AssetImage('assets/images/icon/user.png')
                         : NetworkImage('$img'),
