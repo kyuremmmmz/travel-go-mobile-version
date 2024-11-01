cf560e7a1a2e1f0a44440e0f3dff950185b8a989
Christian David Jasmin <kurosawataki84@gmail.com>
2024-11-01 10:33:52 +0800

    added booking receipt

--- lib/Widgets/Screens/App/hotel_booking.dart
+++ lib/Widgets/Screens/App/hotel_booking.dart
@@ -660,7 +660,7 @@ class _HotelBookingAreaScreenState extends State<HotelBookingAreaScreen> {
                                   focusedBorder: const OutlineInputBorder(
                                       borderSide:
                                           BorderSide(color: Colors.blue)),
-                                  hintText: 'Payment Method',
+                                  hintText: 'Payment Method (Click to Select)',
                                   filled: true,
                                   fillColor: Colors.white,
                                   hintStyle: TextStyle(fontSize: 12.sp),
@@ -695,7 +695,7 @@ class _HotelBookingAreaScreenState extends State<HotelBookingAreaScreen> {
                                   focusedBorder: const OutlineInputBorder(
                                       borderSide:
                                           BorderSide(color: Colors.blue)),
-                                  hintText: 'Room Type',
+                                  hintText: 'Room Type (Click to Select)',
                                   filled: true,
                                   fillColor: Colors.white,
                                   hintStyle: TextStyle(fontSize: 12.sp),
