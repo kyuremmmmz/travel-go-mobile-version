afa3b76f8b8c88034ae0eff2aee21539eb9b19e0
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-02 22:05:30 +0800

    rtwe

--- lib/Controllers/Auth/Oauth.dart
+++ lib/Controllers/Auth/Oauth.dart
@@ -4,9 +4,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Oauth {
   Future<void> nativeGoogleSignIn() async {
-    const webClientId = '203263579620-p7g92bb8c8rdqltr88njobne96j31ob0.apps.googleusercontent.com';
-    const iosClientId = '203263579620-jt01ig40mqkfs9o7t49so0s8kp3jn7qe.apps.googleusercontent.com';
-
+    const webClientId ='203263579620-p7g92bb8c8rdqltr88njobne96j31ob0.apps.googleusercontent.com';
+    const iosClientId ='203263579620-jt01ig40mqkfs9o7t49so0s8kp3jn7qe.apps.googleusercontent.com';
     final GoogleSignIn googleSignIn = GoogleSignIn(
       clientId: iosClientId,
       serverClientId: webClientId,
@@ -15,14 +14,12 @@ class Oauth {
     final googleAuth = await googleUser!.authentication;
     final accessToken = googleAuth.accessToken;
     final idToken = googleAuth.idToken;
-
     if (accessToken == null) {
       throw 'No Access Token found.';
     }
     if (idToken == null) {
       throw 'No ID Token found.';
     }
-
     await supabase.auth.signInWithIdToken(
       provider: OAuthProvider.google,
       idToken: idToken,
