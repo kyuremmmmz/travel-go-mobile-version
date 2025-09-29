aa82a1a50277a3f4078539353d13ddb2a68d5357
Christian David Batrina Jasmin <141408182+kyuremmmmz@users.noreply.github.com>
2025-09-29 08:49:47 +0800

    Update OAuth.dart

--- lib/Controllers/Auth/OAuth.dart
+++ lib/Controllers/Auth/OAuth.dart
@@ -5,7 +5,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class Oauth {
   Future<void> nativeGoogleSignIn() async {
     const webClientId = '203263579620-p7g92bb8c8rdqltr88njobne96j31ob0.apps.googleusercontent.com';
-    const iosClientId = '203263579620-jt01ig40mqkfs9o7t49so0s8kp3jn7qe.apps.googleusercontent.com';
+    const iosClientId = 'IOS CLIENT ID';
 
     final GoogleSignIn googleSignIn = GoogleSignIn(
       clientId: iosClientId,
